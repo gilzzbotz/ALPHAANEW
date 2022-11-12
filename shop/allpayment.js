8b0512517de09ca91ed619d44aa12ea89ad6f832
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-12 16:46:47 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,17 @@
 const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ https://telegra.ph/file/1ebc2c93bb5bdef66202c.jpg
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ https://telegra.ph/file/1ebc2c93bb5bdef66202c.jpg
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ https://telegra.ph/file/1ebc2c93bb5bdef66202c.jpg
 	
-Semuanya atas nama RI****DI
+Scan Qr Di Atas
 
 *📲 PULSA*
 
