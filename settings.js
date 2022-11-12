7ca5276bce25a9994c36ca8207303784ffc9a22b
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-12 16:39:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.ownerNumberr = "6285730794089"
+global.ownerbot = ['6285730794089@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
+global.ownername = "Gilzz XD"
+global.botname = "Gilzz BOTz"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  Gilzz BOT  」\nCreated By Gilzz Aza"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
